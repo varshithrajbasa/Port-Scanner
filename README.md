9e2b376165a8157e2ea72d0240b34fa23a113429
Varshith Raj Basa <varshithraj.basa@gmail.com>
2020-09-05 23:58:45 +0530

    First Commit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Port-Scanner
 Fastest Port Scanner
+
+Usage:
+
+git clone https://github.com/varshithrajbasa/Port-Scanner.git
+
+cd Port-Scanner
+
+./portscanner.py
---
+++ portscanner.py
@@ -0,0 +1,54 @@
+#!/bin/python3
+
+import sys
+import socket
+from time import sleep
+from datetime import datetime
+
+if len(sys.argv) == 2:
+    target = socket.gethostbyname(sys.argv[1])
+
+else:
+    print('Invalid amount of arguments.')
+    print('Syntax: python3 portscanner.py <ip>')
+    print('Exiting the program...')
+    #sys.exit()
+target = '127.0.0.1'
+def portscan():
+    k = 1
+    for port in range(1,65536):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        socket.setdefaulttimeout(1)
+        result = s.connect_ex((target,port))
+        if result == 0:
+            k = k + 1
+            print('Port {} is open '.format(port))
+        scanupdate = 'Scanning port {}'.format(port)
+        print (scanupdate, end='\r')
+        s.close()
+    if k == 1:
+        print('No Ports open.')
+
+print('-'*50)
+print('Scanning target '+target)
+print('Scan Started: '+str(datetime.now()))
+print('-'*50)
+
+try:
+    portscan()
+
+except KeyboardInterrupt:
+    print('\n Exiting Program.')
+
+except socket.gaierror:
+    print('Hostname could not be resolved.')
+    sys.exit()
+
+except socket.error:
+    print('Can not connect to server.')
+    sys.exit()
+
+print('-'*50)
+print('Scan Ended: '+str(datetime.now()))
+print('-'*50)
+
